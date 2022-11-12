bd4b7bf9744d0aee89fb29308181dcf7c241e0af
Divyesh Patel <divyesh.patel@websym.com>
2022-11-12 13:00:30 +0530

    Add project files.

---
+++ DemoColumn/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoColumn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string APIName = "Employee";
+            DataTable dt = new DataTable();
+            DataTable dtColumnMapping = new DataTable();
+            SQLHelper sqlHelper = new SQLHelper();
+            sqlHelper.GetDataTable("GetAllEmployee", dt, CommandType.StoredProcedure);
+
+            SQLHelper sqlHelper1 = new SQLHelper();
+            List<SqlParameter> sp = new List<SqlParameter>()
+            {
+                new SqlParameter() {ParameterName = "@APIName", SqlDbType = SqlDbType.VarChar, Value= APIName}
+            };
+            sqlHelper1.GetDataTable("GetAllColumnMapping", dtColumnMapping, CommandType.StoredProcedure, sp);
+
+            foreach (DataRow dr in dtColumnMapping.Rows)
+            {
+                string col = Convert.ToString(dr["BaseColumn"]);
+                dt.Columns[col].ColumnName = Convert.ToString(dr["DestinationColumn"]);
+                dt.AcceptChanges();
+            }
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
---
+++ DemoColumn/SQLHelper.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoColumn
+{
+    public class SQLHelper
+    {
+        public SqlConnection MySqlConnction;
+        public SqlCommand MySqlCommand;
+        SqlDataAdapter MySqlDataAdapter;
+
+        string sqlConnectionString = Convert.ToString(ConfigurationManager.ConnectionStrings["MasterConnection"]);
+
+        public SQLHelper()
+        {
+            OpenConnction(sqlConnectionString);
+        }
+        
+        #region Private Method
+        private void OpenConnction(string ConnectionString)
+        {
+            try
+            {
+                MySqlConnction = new SqlConnection(ConnectionString);
+                MySqlCommand = new SqlCommand();
+                MySqlCommand.CommandTimeout = 240;
+
+                MySqlDataAdapter = new SqlDataAdapter();
+
+                MySqlConnction.ConnectionString = ConnectionString;
+                MySqlConnction.Open();
+            }
+            catch (Exception)
+            {
+                CloseConnction(MySqlConnction);
+                throw;
+            }
+
+        }
+
+        public void CloseConnction(SqlConnection pConn)
+        {
+            if (pConn != null)
+            {
+                if (pConn.State == System.Data.ConnectionState.Open)
+                {
+                    pConn.Close();
+                    pConn.Dispose();
+                    pConn = null;
+                }
+            }
+        }
+
+        private void DisposeAllConnObjects()
+        {
+            MySqlCommand.Dispose();
+            MySqlCommand = null;
+
+            MySqlDataAdapter.Dispose();
+            MySqlDataAdapter = null;
+
+            CloseConnction(MySqlConnction);
+        }
+        #endregion
+
+        #region FillDataTable
+        public void GetDataTable(string pStrCommandText, DataTable dataTable, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            try
+            {
+                MySqlCommand.CommandType = pEnmCommandType;
+                MySqlCommand.CommandText = pStrCommandText;
+                MySqlCommand.Connection = MySqlConnction;
+
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            MySqlCommand.Parameters.Add(pParamList[i]);
+                    }
+                }
+                MySqlDataAdapter.SelectCommand = MySqlCommand;
+                MySqlDataAdapter.Fill(dataTable);
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            finally
+            {
+                dataTable = null;
+                DisposeAllConnObjects();
+            }
+        }
+        #endregion
+
+        #region FillDataTable
+        public void GetDataSet(string pStrCommandText, DataSet dataSet, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            try
+            {
+                MySqlCommand.CommandType = pEnmCommandType;
+                MySqlCommand.CommandText = pStrCommandText;
+                MySqlCommand.Connection = MySqlConnction;
+
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            MySqlCommand.Parameters.Add(pParamList[i]);
+                    }
+                }
+                MySqlDataAdapter.SelectCommand = MySqlCommand;
+                MySqlDataAdapter.Fill(dataSet);
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            finally
+            {
+                dataSet = null;
+                DisposeAllConnObjects();
+            }
+        }
+        #endregion
+
+        #region SqlDataReader
+        public SqlDataReader ExeReader(string pStrCommandText, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            try
+            {
+                MySqlCommand.CommandType = CommandType.StoredProcedure;
+                MySqlCommand.CommandText = pStrCommandText;
+                MySqlCommand.Connection = MySqlConnction;
+
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    MySqlCommand.CommandType = CommandType.StoredProcedure;
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            MySqlCommand.Parameters.Add(pParamList[i]);
+                    }
+                }
+                else if (pEnmCommandType == CommandType.Text)
+                {
+                    MySqlCommand.CommandType = CommandType.Text;
+                }
+                return MySqlCommand.ExecuteReader();
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            //finally
+            //{
+            //    CloseConnction(MySqlConnction);
+            //}
+        }
+        #endregion
+
+        #region Execute Non Query
+        public int ExeNonQuery(string pStrCommandText, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            int IntResult = 0;
+            try
+            {
+                MySqlCommand.CommandType = CommandType.StoredProcedure;
+                MySqlCommand.CommandText = pStrCommandText;
+                MySqlCommand.Connection = MySqlConnction;
+                MySqlCommand.CommandTimeout = 6000;
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    MySqlCommand.CommandType = CommandType.StoredProcedure;
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            MySqlCommand.Parameters.Add(pParamList[i]);
+                    }
+                }
+                else if (pEnmCommandType == CommandType.Text)
+                {
+                    MySqlCommand.CommandType = CommandType.Text;
+                }
+                IntResult = MySqlCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                //log.WriteLog("ExeNonQuery ===> Message: " + ex.Message + " | StackTrace: " + ex.StackTrace + " | InnerException: " + ex.InnerException);
+                return -1;
+            }
+            finally
+            {
+                DisposeAllConnObjects();
+            }
+            return IntResult;
+        }
+
+        public int ExeNonQueryWithSqlTransaction(string strConnString, string pStrCommandText, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            int IntResult = 0;
+            SqlTransaction objTrans = null;
+
+            using (SqlConnection ConnectionWithSqlTransaction = new SqlConnection(strConnString))
+            {
+                ConnectionWithSqlTransaction.Open();
+                objTrans = ConnectionWithSqlTransaction.BeginTransaction();
+                SqlCommand CmdWithSqlTransaction = new SqlCommand();
+                CmdWithSqlTransaction.Connection = ConnectionWithSqlTransaction;
+                CmdWithSqlTransaction.CommandType = CommandType.StoredProcedure;
+                CmdWithSqlTransaction.CommandText = pStrCommandText;
+                CmdWithSqlTransaction.Transaction = objTrans;
+                CmdWithSqlTransaction.CommandTimeout = 6000;
+
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    CmdWithSqlTransaction.CommandType = CommandType.StoredProcedure;
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            CmdWithSqlTransaction.Parameters.Add(pParamList[i]);
+                    }
+                }
+                else if (pEnmCommandType == CommandType.Text)
+                {
+                    CmdWithSqlTransaction.CommandType = CommandType.Text;
+                }
+
+                try
+                {
+                    IntResult = CmdWithSqlTransaction.ExecuteNonQuery();
+                    objTrans.Commit();
+                }
+                catch (Exception ex)
+                {
+                    objTrans.Rollback();
+                    return -1;
+                }
+                finally
+                {
+                    ConnectionWithSqlTransaction.Close();
+                }
+            }
+            return IntResult;
+        }
+        #endregion
+
+        #region Execute Scalar
+        public string ExeScalar(string pStrCommandText, CommandType pEnmCommandType, List<SqlParameter> pParamList = null)
+        {
+            string StrResult = "";
+            try
+            {
+                MySqlCommand.CommandType = CommandType.StoredProcedure;
+                MySqlCommand.CommandText = pStrCommandText;
+                MySqlCommand.Connection = MySqlConnction;
+
+                if (pEnmCommandType == CommandType.StoredProcedure && pParamList != null)
+                {
+                    MySqlCommand.CommandType = CommandType.StoredProcedure;
+                    for (int i = 0; i < pParamList.Count; i++)
+                    {
+                        if (pParamList[i] != null)
+                            MySqlCommand.Parameters.Add(pParamList[i]);
+                    }
+                }
+                else if (pEnmCommandType == CommandType.Text)
+                {
+                    MySqlCommand.CommandType = CommandType.Text;
+                }
+                StrResult = MySqlCommand.ExecuteScalar().ToString();
+
+            }
+            catch (SqlException ex)
+            {
+                //log.WriteLog("SQLHelper.ExeScalar ===> Message: " + ex.Message + " | StackTrace: " + ex.StackTrace);
+                return "";
+            }
+            finally
+            {
+                DisposeAllConnObjects();
+            }
+            return StrResult;
+        }
+        #endregion
+    }
+}
